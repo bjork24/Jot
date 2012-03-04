7cb122af99333e1bce095e85566fa54bc582d845
Dan Chilton <dan.chilton@gmail.com>
2012-03-04 16:19:53 -0600

    Added entry scaffold, set index as root

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, '4Nx0XHRe9GHDBW1Al9ATZg', '1ZH5QwWxlanIGh4OZf5FbSmI148YPNaC4rYBNPPEPIk'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120304221208) do
+
+  create_table "entries", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
