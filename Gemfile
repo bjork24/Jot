8c4d0982333c5c3c2d33f04ff2f22b55e05d5d81
Dan Chilton <dan.chilton@gmail.com>
2012-03-04 14:10:22 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'mysql2', '0.2.11'
+gem 'jquery-rails', '1.0.12'
+gem "compass", "0.11.7"
+
+group :development do
+  gem 'thin', '1.2.11'
+  gem 'capistrano', '2.6.0'
+  gem 'annotate', '2.4.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jot::Application.config.secret_token = 'b46fb9d9382ce515a75ad5b47196974d74486dd587fff89c477501fb130c5488153710c8afd164bd5d6754bb62f0f23b95051c7fd45539022e9d4358d2c2b9a5'
