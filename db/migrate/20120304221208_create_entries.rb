7a740e2ed76b5f8bdf37a0cb1fe4c5268328fdb3
Dan Chilton <dan.chilton@gmail.com>
2012-03-11 22:39:54 -0500

    Took a comment out of one of the migrations

--- db/migrate/20120304221208_create_entries.rb
+++ db/migrate/20120304221208_create_entries.rb
@@ -4,7 +4,6 @@ class CreateEntries < ActiveRecord::Migration
       t.string :title
       t.text :body
       t.string :note
-      #t.integer :token
       t.references :user
       t.timestamps
     end
