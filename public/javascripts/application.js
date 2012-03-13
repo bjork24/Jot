681d191b473ad13c0d41a02cf805f728eb31cb51
Dan Chilton <dan.chilton@gmail.com>
2012-03-12 23:35:49 -0500

    Just a little delay on the character count

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
+$(function(){
 
   $('#entry_body').keyup(function(){
-    var count = $(this).val().length;
-    var countJar = $('#entry-word-count');
-    if(count <= 475 && countJar.hasClass('warn')) countJar.removeClass('warn');
-    else if(count > 475 && count <= 500) countJar.addClass('warn');
-    else if(count > 500) $(this).val($(this).val().substring(0,500));
-    countJar.text(count);
+    $obj = $(this);
+    delay(function(){
+      var count = $obj.val().length;
+      var countJar = $('#entry-word-count');
+      if(count <= 475 && countJar.hasClass('warn')) countJar.removeClass('warn');
+      else if(count > 475 && count <= 500) countJar.addClass('warn');
+      else if(count > 500) $obj.val($(this).val().substring(0,500));
+      countJar.text(count);
+    }, 500);
   });
 
 });
+
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
