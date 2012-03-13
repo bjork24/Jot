f46a755f9f3ba1500b8cba6f26c985349fda2039
Dan Chilton <dan.chilton@gmail.com>
2012-03-13 09:06:04 -0500

    Left a this reference in my js that was douching things up.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,7 +7,7 @@ $(function(){
       var countJar = $('#entry-word-count');
       if(count <= 475 && countJar.hasClass('warn')) countJar.removeClass('warn');
       else if(count > 475 && count <= 500) countJar.addClass('warn');
-      else if(count > 500) $obj.val($(this).val().substring(0,500));
+      else if(count > 500) $obj.val($obj.val().substring(0,500));
       countJar.text(count);
     }, 500);
   });
